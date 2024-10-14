e0e6fbecb70bec8f5a68ed31fafcbbdb1eac3a32
surdouski <michael.surdouski@gmail.com>
2024-10-14 12:09:05 -0400

    simplify settings.py

--- msf/settings.py
+++ msf/settings.py
@@ -4,10 +4,8 @@
 from pathlib import Path
 
 
-DOT_SETTINGS_DIR_PATH = Path("/.settings")
-DEVICES_SETTINGS_PATH = DOT_SETTINGS_DIR_PATH / "devices.json"
+DEVICES_SETTINGS_PATH = Path("/.settings") / "devices.json"
 
-CONFIG_DIR_PATH = Path("/.config")
-MQTT_AS_CONFIG_PATH = CONFIG_DIR_PATH / "mqtt_as.json"
+MQTT_AS_CONFIG_PATH = Path("/.config") / "mqtt_as.json"
 
 MQTT_DEVICES_TOPIC = "test/devices"  # Do not start mqtt topic's with a "/"
